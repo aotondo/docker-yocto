9fff992a04f70ab2e87b94d04f2f303c78e88847
aotondo <andres@otondo.cl>
2021-08-17 13:34:14 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 if [ -z "$(ls -A /home/yocto/poky/)" ]; then
    echo "-- Downloading Poky from Git --"
-   su yocto
-   cd
-   git clone git://git.yoctoproject.org/poky
-   cd poky
+   chown -R yocto:users /home/yocto/
+   runuser -u yocto git clone git://git.yoctoproject.org/poky /home/yocto/poky/
    echo "-- Checking out Hardknott tag --"
+   cd /home/yocto/poky/
    git checkout -t origin/hardknott -b my-hardknott
-   
+   exit
 fi
 # Ping localhost to keep container alive
 tail -f /var/log/*
+
