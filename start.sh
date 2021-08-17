47aaf607f47a45b4f5f47c2328ea9cc17f0f88da
aotondo <andres@otondo.cl>
2021-08-17 12:55:10 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 if [ -z "$(ls -A /home/yocto/poky/)" ]; then
-   echo Folder of Poky have not been detected, downloading Poky...
+   echo "-- Downloading Poky from Git --"
    su yocto
    cd
    git clone git://git.yoctoproject.org/poky
+   cd poky
+   echo "-- Checking out Hardknott tag --"
+   git checkout -t origin/hardknott -b my-hardknott
+   
 fi
 # Ping localhost to keep container alive
-ping localhost
+tail -f /var/log/*
